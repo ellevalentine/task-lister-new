76ab14c00b018edba655500f5a7db578af95f843
ellevalentine <elle.valentine@hotmail.com>
2019-05-14 16:25:08 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const formEl = document.getElementById('create-task-form');
+const listBody = document.getElementById('tasks');
+
+
+function addToList (desc) {
+  const newTask = document.createElement('newTask');
+
+  newTask.innerHTML = `
+    <li> ${desc} <button class='delete-btn'>X</button> </li> 
+    `
+
+  listBody.append(newTask)
+
+  const deleteBtn = newTask.querySelector('.delete-btn');
+  deleteBtn.addEventListener('click', function(event) {
+    newTask.remove();
+  });
+};
+
+
+formEl.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const todo = formEl.description.value;
+
+  addToList(todo);
+  });
